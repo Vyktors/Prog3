e0f5cf8e59215ea843e7452c696fae49e9a4c958
Stephane <guerinstephane17@gmail.com>
2017-10-15 12:21:47 -0400

    update prob recursivite

--- Semaine 7 - Recursivite/Lab5_recursivite_1_MVGu/Lab5_recursivite_1_MVGu/probleme1.cs
+++ Semaine 7 - Recursivite/Lab5_recursivite_1_MVGu/Lab5_recursivite_1_MVGu/probleme1.cs
@@ -15,58 +15,139 @@ namespace Lab5_recursivite_1_MVGu
         bool chkLigne;
         bool chkCol;
         bool chkDia;
+        int CptBonneSolution;
 
         public probleme1()
         {
-
-            grid = new int[SIZE,SIZE];
+            CptBonneSolution = 0;
+            grid = new int[SIZE, SIZE];
             //UnitTEST - WORKING 
             /*grid[0, 0] = 1;
             grid[0, 3] = 1;
-            
+
             for (int i = 0; i < SIZE; i++)
             {
-                chkCol = Chk_Securite_Colonne(1,i,i,grid);
+                chkCol = Chk_Securite_Colonne(1, i, i, grid);
                 chkLigne = Chk_Securite_Ligne(1, i, i, grid);
                 chkDia = Chk_Securite_Diagonale(1, i, i, grid);
             }
             */
-              
+
+
         }
 
         public void Start()
         {
-            Place_Reine_Securite(1, 0, 0, grid);
+            Console.Clear();
+            //   Parcourir les colonnes du damier
+            for (int i = 0; i < SIZE; i++)
+            {
+                if (Place_Reine_Securite(1, i, 0, grid))
+                {
+                    Console.WriteLine("Colonne " + i + " a au moins une solution");
+                }
+
+                else
+                {
+                    Console.WriteLine("Colonne " + i + " n'a pas de solution");
+                }
+            }
+            
 
         }
-        
 
+        /* Place_Reine_Securité
+             Place une reine en sécurité et retourne VRAI
+             Sinon passe à la prochaine ligne de la colonne
+             Si à la limite du nombre de ligne de la colonne alors pas de solution retourne FAUX 
+
+             Si les enfants retournent VRAI alors MEMORISE LA SOLUTION et passe à la prochaine ligne
+
+        */
         private bool Place_Reine_Securite(int _NoSln, int _col, int _ligne, int[,] _grid)
+
         {
-            int ligneTest = _ligne;
-            if(_col >= SIZE|| _ligne >= SIZE)
+          
+
+            if (_col >= SIZE || _ligne >= SIZE)
             {
-                //2DO: Gestion d'erreur à revoir, comment arrêter le programme si erreur.
-                Console.Clear();
-                Console.WriteLine("ERREUR SORTI DU TABLEAU 17685");                
+                // Sortie du tableau alors considère la solution fausse et l'appel en erreur
+                Console.WriteLine("Indice hors tableau avec solution no: " + _NoSln);
                 return false;
             }
 
-            while(!(Chk_Securite_Colonne(_NoSln,_col,_ligne,_grid) && Chk_Securite_Ligne(_NoSln, _col, _ligne, _grid) && Chk_Securite_Diagonale(_NoSln, _col, _ligne, _grid)))
+            /*
+             * Si Position en sécurité vérifie les enfants de cette solution et passe à la prochaine colonne
+             * sinon Si dernière ligne alors pas de solution retourne FAUX
+             *      sinon Test la prochaine ligne
+             *      
+             */
+            
+
+            if (Chk_Securite_Colonne(_NoSln, _col, _ligne, _grid)
+                && Chk_Securite_Ligne(_NoSln, _col, _ligne, _grid) 
+                && Chk_Securite_Diagonale(_NoSln, _col, _ligne, _grid))
             {
-                //TANT QUE Colonne, Ligne et Diagonale sont OK.
-                
+                //  En situation sécuritaire, donc on présume solution viable jusqu'à preuve du contraire
+                _grid[_col, _ligne] = _NoSln;
 
+                if (_ligne + 1 >= SIZE)
+                {
+                    // Je suis en sécurité sur la dernière ligne de la colonne alors solution viable  
+                    CptBonneSolution++;
+                    Imprime_Solution(_NoSln, _grid);
+                    return true;
+                }
+                else if(Place_Reine_Securite(_NoSln, _col + 1, 0, _grid))
+                {
+                    // Implique que tous les enfants sont vrais donc la solution est vrai
+                    // donc on imprime la solution
+                    return true;
+
+                }
+                else
+                {
+                    // Implique qu'il n'y a pas de solution dans les enfants
+                    // Retour faux
+                    _grid[_col, 0] = 0;
+                    return false;
+
+                }
+               
             }
+            else
+            {
+                // La reine n'est pas en sécurité
+                if (_ligne + 1 >= SIZE)
+                {
+                    // Fin de la colonne pas d'autre solution
+                    return false;
+                }
+                //  Pas en sécurité, pas besoin de vérifier les enfants, passe à la prochaine ligne de la colonne
+                else if(Place_Reine_Securite(_NoSln, _col,_ligne+1, _grid))
 
-            _grid[_col, ligneTest] = _NoSln;
+                {
+                    // Implique que tous les enfants sont vrais donc la solution est vrai
 
+                    return true;
 
+                }
+                else
+                {
+                    // Implique qu'il n'y pas de solution chez les enfants
+                    return false;
+                }
+               
+            }
+    }
 
-            //DEBUG
+        private bool Imprime_Solution(int _NoSln, int[,] _grid)
+        {
+
+            Console.WriteLine("Solution " + _NoSln + "  Cumulatif: " + CptBonneSolution);
+            
             return true;
         }
-
         private bool Chk_Securite_Ligne(int NoSln, int _col, int _ligne, int[,] _grid)
         {
             for (int i = 0; i < SIZE; i++)
