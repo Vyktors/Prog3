102dd21e4a7da4dac7a56daae43039937f0ee3f9
Stephane <guerinstephane17@gmail.com>
2017-10-17 18:11:55 -0400

    Version de SGu

--- Semaine 7 - Recursivite/Lab5_recursivite_1_MVGu/Lab5_recursivite_1_MVGu/probleme1.cs
+++ Semaine 7 - Recursivite/Lab5_recursivite_1_MVGu/Lab5_recursivite_1_MVGu/probleme1.cs
@@ -10,7 +10,7 @@ namespace Lab5_recursivite_1_MVGu
 {
     class probleme1
     {
-        const int SIZE = 8;
+        const int SIZE = 3;
         int[,] grid;
         bool chkLigne;
         bool chkCol;
@@ -38,19 +38,40 @@ namespace Lab5_recursivite_1_MVGu
 
         public void Start()
         {
-            Console.Clear();
+            int NoSolution;
+
+            // Pour éviter les colisions entre les valeurs des indices du tableau et la valeur de la solution
+            NoSolution = SIZE * 1000;
+
+            //Console.Clear();
+
+            if (Imprime_Solution(-1, grid)) { };
+
+
+
+
             //   Parcourir les colonnes du damier
             for (int i = 0; i < SIZE; i++)
             {
-                if (Place_Reine_Securite(1, i, 0, grid))
+                NoSolution = i + SIZE * 1000;
+                if (Initialise_Grille(0, grid));
+                Console.WriteLine("-------------------------");
+
+                Console.WriteLine("Solution: " + NoSolution);
+
+                Console.WriteLine("-------------------------");
+                if (Place_Reine_Securite(NoSolution, 0, i, grid))
                 {
                     Console.WriteLine("Colonne " + i + " a au moins une solution");
+
                 }
 
                 else
                 {
                     Console.WriteLine("Colonne " + i + " n'a pas de solution");
-                }
+                    if (Imprime_Solution(-1, grid)) ;
+                };
+                Console.WriteLine("-------------------------");
             }
             
 
@@ -68,10 +89,9 @@ namespace Lab5_recursivite_1_MVGu
 
         {
           
-
             if (_col >= SIZE || _ligne >= SIZE)
             {
-                // Sortie du tableau alors considère la solution fausse et l'appel en erreur
+                // Hors tableau alors considère la solution fausse et l'appel en erreur
                 Console.WriteLine("Indice hors tableau avec solution no: " + _NoSln);
                 return false;
             }
@@ -88,28 +108,36 @@ namespace Lab5_recursivite_1_MVGu
                 && Chk_Securite_Ligne(_NoSln, _col, _ligne, _grid) 
                 && Chk_Securite_Diagonale(_NoSln, _col, _ligne, _grid))
             {
-                //  En situation sécuritaire, donc on présume solution viable jusqu'à preuve du contraire
+
+                //  En situation sécuritaire on présume la solution viable
                 _grid[_col, _ligne] = _NoSln;
 
-                if (_ligne + 1 >= SIZE)
+                if (_ligne + 1 >= SIZE || _col + 1 >= SIZE)
                 {
-                    // Je suis en sécurité sur la dernière ligne de la colonne alors solution viable  
+                    /* La position est en sécurité sur la dernière ligne de la colonne ou de la dernière colonne de la ligne 
+                     alors toutes la chaine de solution viable
+                    */
+
                     CptBonneSolution++;
                     Imprime_Solution(_NoSln, _grid);
+                    Console.WriteLine("Imprime tous le tableau");
+                    Imprime_Solution(-1, _grid);
+
                     return true;
                 }
                 else if(Place_Reine_Securite(_NoSln, _col + 1, 0, _grid))
                 {
                     // Implique que tous les enfants sont vrais donc la solution est vrai
                     // donc on imprime la solution
+
                     return true;
 
                 }
                 else
                 {
                     // Implique qu'il n'y a pas de solution dans les enfants
-                    // Retour faux
-                    _grid[_col, 0] = 0;
+                    // Efface la réponse et retourne faux à la chaine d'appel
+                    _grid[_col, _ligne] = 0;
                     return false;
 
                 }
@@ -141,10 +169,54 @@ namespace Lab5_recursivite_1_MVGu
             }
     }
 
-        private bool Imprime_Solution(int _NoSln, int[,] _grid)
+        private bool Initialise_Grille(int _Valeur, int[,] _grid)
         {
 
-            Console.WriteLine("Solution " + _NoSln + "  Cumulatif: " + CptBonneSolution);
+            for (int iColonne = 0; iColonne < SIZE; iColonne++)
+            {
+                for (int iLigne = 0; iLigne < SIZE; iLigne++)
+                {
+                     _grid[iColonne, iLigne]= _Valeur;
+                };
+
+            };
+
+            return true;
+        }
+
+        private bool Imprime_Solution(int _NoSln, int[,] _grid)
+        {
+        
+
+            if (_NoSln == -1)
+            {
+                Console.WriteLine("Imprime l'ensemble du tableau");
+                for (int iColonne = 0; iColonne < SIZE; iColonne++)
+                {
+                    for (int iLigne = 0; iLigne < SIZE; iLigne++)
+                    {
+                            Console.WriteLine("Valeur : [" + iColonne + "," + iLigne + "] = " + _grid[iColonne, iLigne]);
+                    };
+
+                };
+            }
+            else
+            {
+                Console.WriteLine("Solution " + _NoSln + "  Cumulatif: " + CptBonneSolution);
+
+                for (int iColonne = 0; iColonne < SIZE; iColonne++)
+                {
+                    for (int iLigne = 0; iLigne < SIZE; iLigne++)
+                    {
+                        if (_grid[iColonne, iLigne] == _NoSln)
+                        {
+                            Console.WriteLine("Reine à: [" + iColonne + "," + iLigne + "]");
+                        };
+
+                    };
+
+                };
+            };
             
             return true;
         }
